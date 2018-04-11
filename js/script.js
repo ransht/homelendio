38a40d9e8f0479980bd1d3b1975159e486247cae
ransht <ransht@gmail.com>
2018-04-11 23:19:08 +0300

    tx

--- js/script.js
+++ js/script.js
@@ -67,9 +67,11 @@ $(function() {
   
 	  $( '.go_to' ).click( function(e){ // ловим клик по ссылке с классом go_to
 		  var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+		  $('.mobile-menu').hide();
 		  if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
 			  $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);// анимируем скроолинг к элементу scroll_el
 		  } 
+		  
 		  e.preventDefault();
 	  });
   
